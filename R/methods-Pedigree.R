0883bd998c4c9ba1dc65c03f6f111ea16f170ef8
Samuel Younkin <syounkin@jhsph.edu>
2012-12-05 16:23:37 -0500

    Cleaned up methods-Pedigree.R

--- R/methods-Pedigree.R
+++ R/methods-Pedigree.R
@@ -9,15 +9,13 @@ setMethod("initialize", signature(.Object="Pedigree"),
 		  callNextMethod(.Object, trios=trios, trioIndex=trioIndex, ...)
 	  })
 
-setMethod("show", "Pedigree", function(object){ print(head(object@trios) ) } )
-
-setMethod("trios", signature(object="Pedigree"),
-	  function(object) object@trios)
-
+setMethod("show", signature(object="Pedigree"), function(object){ print(head(object@trios) ) } )
+#~~~~~~~~~~~~~~~~
+setMethod("trios", signature(object="Pedigree"), function(object) object@trios)
+setMethod("trioIndex", signature(object="Pedigree"), function(object) object@trioIndex)
+#~~~~~~~~~~~~~~~~
 setMethod("offspringNames", signature(object="Pedigree"), function(object) trios(object)$O)
 setMethod("fatherNames", signature(object="Pedigree"), function(object) trios(object)$F)
 setMethod("motherNames", signature(object="Pedigree"), function(object) trios(object)$M)
 setMethod("sampleNames", signature(object="Pedigree"), function(object) offspringNames(object))
 setMethod("allNames", signature(object="Pedigree"), function(object) unique(trioIndex(object)$individualId))
-setMethod("trioIndex", signature(object="Pedigree"), function(object) object@trioIndex)
-
