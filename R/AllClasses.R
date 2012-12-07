d107a8b9ef26578522b918272e18b62b84e5e6c9
Rob Scharpf <rscharpf@jhsph.edu>
2012-12-07 08:17:52 -0500

    Add TrioSet and TrioSetList definitions

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -1,31 +1,24 @@
 setClass("Pedigree", representation(trios="data.frame", trioIndex="data.frame"))
 
-#setOldClass("ff_array")
-#setOldClass("ff_matrix")
-#setClass("LogRratioSet", contains="eSet")
-#setClassUnion("matrixOrNULL", c("matrix", "NULL"))
-#setClassUnion("arrayORff_array", c("array", "ff_array"))
-
-
-
+setOldClass("ff_array")
+setOldClass("ff_matrix")
+setClass("LogRratioSet", contains="eSet")
+setClassUnion("matrixOrNULL", c("matrix", "NULL"))
+setClassUnion("arrayORff_array", c("array", "ff_array"))
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #~~~~ TrioSet Class ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-## setClass("TrioSet", contains="gSet",
-## 	 representation(fatherPhenoData="AnnotatedDataFrame",
-## 			motherPhenoData="AnnotatedDataFrame",
-## 			pedigree="Pedigree",
-## 			mindist="matrixOrNULL"))
+ setClass("TrioSet", contains="gSet",
+ 	 representation(fatherPhenoData="AnnotatedDataFrame",
+ 			motherPhenoData="AnnotatedDataFrame",
+ 			pedigree="Pedigree",
+ 			mindist="matrixOrNULL"))
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #~~~~ TrioSetList Class ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-## setClass("TrioSetList", contains="gSetList",
-## 	 representation(pedigree="Pedigree",
-## 			##assayDataList="AssayData",
-## 			##phenoData="AnnotatedDataFrame",
-## 			fatherPhenoData="AnnotatedDataFrame",
-## 			motherPhenoData="AnnotatedDataFrame"))
-## 			##featureDataList="list",
-## 			##chromosome="integer"))
+ setClass("TrioSetList", contains="gSetList",
+ 	 representation(pedigree="Pedigree",
+ 			fatherPhenoData="AnnotatedDataFrame",
+ 			motherPhenoData="AnnotatedDataFrame"))
