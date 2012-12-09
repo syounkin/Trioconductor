0eb0e723f72639ebe09fe8e0a1ef5426db87da0f
Samuel Younkin <syounkin@jhsph.edu>
2012-12-09 12:07:44 -0500

    Cleaned up file functions.R

--- R/functions.R
+++ R/functions.R
@@ -1,12 +1,3 @@
-make.unique2 <- function(names, sep="___DUP") make.unique(names, sep)
-originalNames <- function(names){
-	if(length(names) ==0) return(names)
-	sep <- formals(make.unique2)[["sep"]]
-	index <- grep(sep, names)
-	if(length(index) > 0) names[index] <- sapply(names[index], function(x) strsplit(x, sep)[[1]][[1]])
-	names
-}
-
 completeTrios.fn <- function( ped, id.vec ){
   if( !is.character(id.vec) ) stop( "ID vector must be character class.")
   index.vec <- which( colSums( apply( trios(ped), 1, FUN = function( row, id = id.vec ){ row %in% id } ) ) == 3 )
@@ -40,4 +31,14 @@ geno.mat.fn <- function( ts, type = "holger" ){
   }else{
     stop( "What format type would youlike for output?" )
   }
+}
+
+make.unique2 <- function(names, sep="___DUP") make.unique(names, sep)
+
+originalNames <- function(names){
+	if(length(names) ==0) return(names)
+	sep <- formals(make.unique2)[["sep"]]
+	index <- grep(sep, names)
+	if(length(index) > 0) names[index] <- sapply(names[index], function(x) strsplit(x, sep)[[1]][[1]])
+	return( names )
 }
