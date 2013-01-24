80c6947ba762bf51fc4954a0e8a45f2d227accf2
Samuel Younkin <syounkin@jhsph.edu>
2013-01-24 09:40:30 -0500

    First commit of the SummarizedExperiment based package.

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -1,34 +1,35 @@
-setClass("Pedigree", representation(trios="data.frame", trioIndex="data.frame"))
+setClass("SNPExperiment", contains="SummarizedExperiment", representation(Pedigree="Pedigree") )
+##setClass("Pedigree", representation(trios="data.frame", trioIndex="data.frame"))
 
-setOldClass("ff_array")
-setOldClass("ff_matrix")
-setClass("LogRratioSet", contains="eSet")
-#setClassUnion("matrixOrNULL", c("matrix", "NULL"))
-setClassUnion("arrayORff_array", c("array", "ff_array"))
+## setOldClass("ff_array")
+## setOldClass("ff_matrix")
+## setClass("LogRratioSet", contains="eSet")
+## #setClassUnion("matrixOrNULL", c("matrix", "NULL"))
+## setClassUnion("arrayORff_array", c("array", "ff_array"))
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#~~~~ TrioSet Class ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-setClass("tSet", contains="gSet",
-	 representation(pedigree="Pedigree", "VIRTUAL"))
+## #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## #~~~~ TrioSet Class ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## setClass("tSet", contains="gSet",
+## 	 representation(pedigree="Pedigree", "VIRTUAL"))
 
-setClass("TdtSet", contains="tSet")
+## setClass("TdtSet", contains="tSet")
 
 
-setClass("gTrio", contains="gSet",
- 	 representation(fatherPhenoData="AnnotatedDataFrame",
- 			motherPhenoData="AnnotatedDataFrame",
- 			pedigree="Pedigree", map="data.frame" ))
+## setClass("gTrio", contains="gSet",
+##  	 representation(fatherPhenoData="AnnotatedDataFrame",
+##  			motherPhenoData="AnnotatedDataFrame",
+##  			pedigree="Pedigree", map="data.frame" ))
 
-setClass("iTrio", contains="gSet",
- 	 representation(fatherPhenoData="AnnotatedDataFrame",
- 			motherPhenoData="AnnotatedDataFrame",
- 			pedigree="Pedigree" ))
+## setClass("iTrio", contains="gSet",
+##  	 representation(fatherPhenoData="AnnotatedDataFrame",
+##  			motherPhenoData="AnnotatedDataFrame",
+##  			pedigree="Pedigree" ))
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#~~~~ TrioSetList Class ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- setClass("TrioSetList", contains="gSetList",
- 	 representation(pedigree="Pedigree",
- 			fatherPhenoData="AnnotatedDataFrame",
- 			motherPhenoData="AnnotatedDataFrame"))
+## #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## #~~~~ TrioSetList Class ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##  setClass("TrioSetList", contains="gSetList",
+##  	 representation(pedigree="Pedigree",
+##  			fatherPhenoData="AnnotatedDataFrame",
+##  			motherPhenoData="AnnotatedDataFrame"))
