e6b63c4cc185248a4ed2d3287fe32497ba213acb
Samuel Younkin <syounkin@jhsph.edu>
2013-03-05 16:05:33 -0500

    Created trio method and cleaned up PedClass constructor

--- R/methods-PedClass.R
+++ R/methods-PedClass.R
@@ -4,9 +4,22 @@ setMethod("initialize", signature(.Object="PedClass"),
            callNextMethod(.Object, ... )
          })
 
-setMethod( "PedClass", signature(object = "DataFrame"), function(object) new("PedClass", object) )
+setMethod( "PedClass", signature(object = "DataFrame"), function(object) {
+  ped.DF <- with( as(object, "data.frame"), {
+    DataFrame( famid = factor(famid),
+              id = factor(id),
+              fid = factor(fid),
+              mid = factor(mid),
+              sex = factor(sex),
+              dx = factor(dx) )
+  })
+  new("PedClass", ped.DF)
+})
 
-## setMethod("pedigree",  signature(object="PedClass"), function(object) object@pedigree )
+setMethod("trios",  signature(object="PedClass"), function(object) {
+  trio.df <- subset( as(object, "data.frame" ), !is.na(fid) & !is.na(mid) & !is.na(id) )
+  trio.df[,c("id","fid","mid")]
+})
 
 ## setMethod("ids",  signature(object="PedClass"), function(object){
 ##   ped.df <- pedigree(object)
