a0b5c63a527e976318d3a2ff2b53715c2cf88ec2
Samuel Younkin <syounkin@jhsph.edu>
2013-04-10 12:08:00 -0400

    Fixed a redundnacy in CNVMatrix method

--- R/methods-GRanges.R
+++ R/methods-GRanges.R
@@ -1,10 +1,9 @@
 setMethod("CNVMatrix",  signature(object="GRanges"), function(object){
   id.vec <- as.character(unique(values(object)$id))
-  deletion <- object[values(object)$cn < 3 ]
-  cmp <- disjoin(deletion)
+  cmp <- disjoin(object)
   cnv <- matrix(NA, nrow = length(id.vec), ncol = length(cmp) )
   for ( i in 1:length(id.vec) ){
-    cnv[i,] <- countOverlaps( cmp, reduce(deletion[values(deletion)$id == id.vec[i]]) )
+    cnv[i,] <- countOverlaps( cmp, reduce(object[values(object)$id == id.vec[i]]) )
   }
   rownames(cnv) <- id.vec
   colnames(cnv) <- paste0("comp",1:length(cmp))
