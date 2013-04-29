90a00e06d9cb86c7e65f206f5e1a7c767ca75dd2
Samuel Younkin <syounkin@jhsph.edu>
2013-04-29 14:09:56 -0400

    Updated CNVMatrix() and CNVMatrix vignette.

--- R/methods-GRanges.R
+++ R/methods-GRanges.R
@@ -1,9 +1,9 @@
 setMethod("CNVMatrix",  signature(object="GRanges"), function(object){
-  id.vec <- as.character(unique(values(object)$id))
+  id.vec <- as.character(unique(names(object)))
   cmp <- disjoin(object)
   cnv <- matrix(NA, nrow = length(id.vec), ncol = length(cmp) )
   for ( i in 1:length(id.vec) ){
-    cnv[i,] <- countOverlaps( cmp, object[values(object)$id == id.vec[i]] )
+    cnv[i,] <- countOverlaps( cmp, object[names(object) == id.vec[i]] )
   }
   rownames(cnv) <- id.vec
   colnames(cnv) <- paste0("comp",1:length(cmp))
