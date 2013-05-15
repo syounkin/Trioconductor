5f4161d91fd2ad70a885d8229f50b0dbf73ada21
Samuel Younkin <syounkin@jhsph.edu>
2013-05-15 18:16:23 -0400

    Trio of ducks in welcome message.

--- R/zzz.R
+++ R/zzz.R
@@ -2,9 +2,11 @@ THISPKG <- "trioClasses"
 .onAttach <- function(libname, pkgname) {
 	version <- packageDescription("trioClasses", field="Version")
 	packageStartupMessage(paste("
-====================================
 Welcome to trioClasses version ", version, "\n",
-"====================================", sep = "" ) )
+"~~~~~~~~~~~~~~~~~", "\n",
+">()_  >()_  >()_ ", "\n",
+" (__)  (__)  (__)", "\n",
+"~~~~~~~~~~~~~~~~~", sep = "" ) )
 }
 
 .onUnload <- function(libpath){
