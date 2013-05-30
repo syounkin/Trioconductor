aa07fd3e11b3106894bfa1ea2f7342d6bb55cbdc
Rob Scharpf <rscharpf@jhsph.edu>
2013-05-29 23:01:05 -0400

    Add constructor for PedClass

--- R/methods-PedClass.R
+++ R/methods-PedClass.R
@@ -4,17 +4,40 @@ setMethod("initialize", signature(.Object="PedClass"),
            callNextMethod(.Object, ... )
          })
 
-setMethod( "PedClass", signature(object = "DataFrame"), function(object) {
-  ped.DF <- with( as(object, "data.frame"), {
-    DataFrame( famid = factor(famid),
-              id = factor(id),
-              fid = factor(fid),
-              mid = factor(mid),
-              sex = factor(sex),
-              dx = factor(dx) )
-  })
-  new("PedClass", ped.DF)
+setValidity("PedClass", function(object){
+	if(!all(c("famid", "id", "fid", "mid", "sex", "dx") %in% colnames(object))){
+		msg <- "PedClass must have columns 'famid', 'id', 'fid', 'mid', 'sex', and 'dx'"
+	} else msg <- TRUE
+	msg
 })
+
+##setMethod( "PedClass", signature(object = "DataFrame"), function(object) {
+##  ped.DF <- with( as(object, "data.frame"), {
+##    DataFrame( famid = factor(famid),
+##              id = factor(id),
+##              fid = factor(fid),
+##              mid = factor(mid),
+##              sex = factor(sex),
+##              dx = factor(dx) )
+##  })
+##  new("PedClass", ped.DF)
+##})
+
+## trivial constructor...assumes object is DataFrame
+PedDataFrame <- function(){
+    DataFrame(famid = factor(),
+              id = factor(),
+              fid = factor(),
+              mid = factor(),
+              sex = factor(),
+              dx = factor())
+}
+
+## constructor
+PedClass <- function(object){
+	if(missing(object)) object <- PedDataFrame()
+	new("PedClass", object)
+}
 
 setMethod("trios",  signature(object="PedClass"), function(object) {
   trio.df <- subset( as(object, "data.frame" ), !is.na(fid) & !is.na(mid) & !is.na(id) )
