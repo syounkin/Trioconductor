d75726754d3b9612cee718c9838ac8441087725e
Samuel Younkin <syounkin@jhsph.edu>
2013-06-17 15:27:52 -0400

    Simple update of raw data vignettes

--- vignettes/build.sh
+++ vignettes/build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-name="inherited-deletions"
+# name="inherited-deletions"
+name="rawData-chr7"
+cd ~/trioClasses/vignettes/
+pandoc -o $name.html $name.md
+cp $name.html ~/sgy-website/vignettes/
+rsync -a ~/trioClasses/vignettes/figures/ ~/sgy-website/vignettes/figures/
+cd ~/sgy-website/
+./build-website.sh
+name="rawData"
 cd ~/trioClasses/vignettes/
 pandoc -o $name.html $name.md
 cp $name.html ~/sgy-website/vignettes/
